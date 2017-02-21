c69d4d42719b9aadfa1e172e2a1cfaba9630312f
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-21 22:15:50 +0400

    + bytecode/ FORTH machine compiler/VM runtime
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bytecode/hpp.hpp
+++ bytecode/hpp.hpp
@@ -2,10 +2,20 @@
 #define _H_HPP
 
 #include <iostream>
-#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <vector>
 using namespace std;
 
-struct Sym {};
+struct Sym { string val; Sym(string); virtual string dump(); };
+
+struct Saver:Sym { Saver(string); string dump(); };
+
+struct Cmd0:Sym { Cmd0(string V,uint8_t OP);
+	int addr; uint8_t op; string dump(); };
+struct Nop:Cmd0 { Nop(string V); };
+struct Halt:Cmd0 { Halt(string V); };
 
 extern int yylex();
 extern int yylineno;
@@ -15,4 +25,17 @@ extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
 
+#define Dsz 0x10
+extern int Dp;
+extern Sym* D[Dsz];
+#define Rsz 0x100
+extern int Rp;
+extern int R[Rsz];
+#define Msz 0x1000
+extern int Ip;
+extern int Mp;
+extern uint8_t M[Msz];
+extern int VM(int,char**);
+
 #endif // _H_HPP
+
