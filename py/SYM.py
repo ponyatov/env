f1fd2bf82b820af606e8981fb93df3afadc9b6c0
Dmitry Ponyatov <dponyatov@gmail.com>
2017-02-27 00:32:32 +0400

    + pytest
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py/SYM.py
+++ py/SYM.py
@@ -3,6 +3,3 @@ class Sym:
         self.tag = T ; selv.val = V     # <T:V> pair
         self.nest = []                  # nest[]ed elements
         self.attr = {}                  # attr{}ibutes
-
-if __name__='__main__':
-    print __name__
