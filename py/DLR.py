ded63c10dbf23ca5580c0dde0a3104e350a22469
Dmitry Ponyatov <dponyatov@gmail.com>
2017-03-08 15:36:15 +0400

    + py/DLR 42
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py/DLR.py
+++ py/DLR.py
@@ -1,15 +1,36 @@
+# # import operator as op
+ 
 from parsimonious.grammar import Grammar
+from __builtin__ import str
 
+# print Grammar(\n')['program'].parse('42')
+ 
 class Mini:
-    def parse(self, src):
-        grammar = ''
-        return Grammar(grammar)
+     
+#     def __init__(self, env={}): self.env = env
+#     
+    def parse(self, source):
+#         # select grammar definition using __doc__strings introspection
+        grammar = '\n'.join(j.__doc__
+            for i, j in self.__class__.__dict__.items()
+            if not i.startswith('__') and j.__doc__)
+        print grammar
+        return Grammar(grammar)['program'].parse(source)
 
+    def eval(self, source):
+        node = self.parse(source) if isinstance(source, str) else source
+        method = getattr(self, node.expr_name)
+        return method(node, [self.eval(n) for n in node])
+     
+    def program(self, node, children):
+        r' program = ~"[0-9]+" '
+        return int(node.text)
+ 
 def test_hello():
     assert True
-
+ 
 def test_class():
     assert Mini() != Mini()
     
-# def test_parse_none():
-print Mini().parse('')
+def test_numbers():
+    assert Mini().eval('42') == 42
