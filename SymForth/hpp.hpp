6d55efc7becdc20eb60eaf757a305f1dc7b122aa
Dmitry Ponyatov <dponyatov@gmail.com>
2017-03-13 17:08:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- SymForth/hpp.hpp
+++ SymForth/hpp.hpp
@@ -2,16 +2,25 @@
 #define _H_HPP
 
 #include <iostream>
+#include <sstream>
 #include <cstdlib>
+#include <vector>
+#include <map>
 using namespace std;
 
 struct Sym {
 	string tag,val;
 	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*); void clear();
 	virtual string head();
 	string pad(int);
 	virtual string dump(int=0);
 };
+
+struct Int:Sym { Int(string); long val; string head(); };
+struct Num:Sym { Num(string); double val; string head(); };
+
+struct Fn:Sym { Fn(string); };
 
 extern int yylex();
 extern int yylineno;
@@ -20,5 +29,9 @@ extern char* yytext;
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
+
+// FORTH machine
+
+extern Sym stack;
 
 #endif // _H_HPP
