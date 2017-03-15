4ba8a37f5f7105b532b5ca43a29e266b8570cd70
Dmitry Ponyatov <dponyatov@gmail.com>
2017-03-15 10:57:12 +0400

    + JVM/ run from make src->log
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- JVM/Mumbler.java
+++ JVM/Mumbler.java
@@ -1,5 +1,10 @@
+// Mumbler Lisp @
 // http://cesquivias.github.io/blog/2014/10/13/writing-a-language-in-truffle-part-1-a-simple-slow-interpreter/
-	
+
 class Mumbler {
-	public static void main(String[] args) { System.out.println("Hello"); }
+	public static void main(String[] args) {
+		if (args.length != 1)
+			throw new IllegalArgumentException("usage: java Mumber source.file");
+		
+	}
 }
